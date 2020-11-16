e31dcafe4bdb5c9b0399e5a73b1e63ad4ada3e09
Jesse Gohl <jesse.gohl@modelon.com>
2020-11-16 09:30:43 -0600

    Script cleanup

--- LibraryConfig/Jenkinsfile-default.groovy
+++ LibraryConfig/Jenkinsfile-default.groovy
@@ -1,11 +1,9 @@
 import groovy.json.JsonBuilder;
 import groovy.json.JsonSlurper;
 import org.yaml.snakeyaml.Yaml;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 
-// Only keep the last five builds (for space reasons)
+// Only keep the last 50 builds (for space reasons)
 properties([[$class: 'BuildDiscarderProperty',
 	strategy: [
 		$class: 'LogRotator',
@@ -16,46 +14,41 @@ properties([[$class: 'BuildDiscarderProperty',
 	(env.TRIGGER_CAUSE == null ? "MANUAL" : env.TRIGGER_CAUSE) +
 	")"
 	node {
-		def yamlPaths = [:]
 		def baseDir = pwd()
 	
-		// The following must be customized for the tests and build environment
-		def oct_home="C:\\programs\\OCT\\edinburgh\\stable"
-		def mtt_home="C:\\Users\\JesseGohl\\projects\\P539-MTT\\Installations"
+		// The following must be customized for the tests and build environment 
 		def mtt_egg="mtt-3.0.0-py3.7.egg"
-		def repo_path = 'https://github.com/jesse-gohl/ExampleTestLibrary.git' 
+		def repo_path = "https://github.com/jesse-gohl/ExampleTestLibrary.git" 
 		def yaml_file="LibraryConfig/test_mtt_default.yaml"
 		
 		stage("Setup") {
 			// Checkout run configuration
-			doCheckout(repo_path, 'master', 'Runconfigs', '')
-			
-			// Find what to checkout, parse the run configuration
-			def cfgPaths = readYaml file: yaml_file
-			yamlPaths = cleanPaths(baseDir, cfgPaths)
+			doCheckout(repo_path, 'master', 'Runconfigs', '')			
 
 			// Convert the tests
-			def scriptToRun = generateConvBatCall(baseDir, oct_home, mtt_home, mtt_egg, yaml_file)
+			def scriptToRun = generateConvBatCall(baseDir, mtt_egg, yaml_file)
 			bat scriptToRun
 		}
 		
 		stage("Test") {
 			// Run the tests
-			def scriptToRun = generateTestBatCall(baseDir, oct_home, mtt_home, mtt_egg)
+			def scriptToRun = generateTestBatCall(baseDir, mtt_egg)
 			bat scriptToRun
 		}
 		
 		stage("Archive") {	
 			// Get the result paths
-			def mypaths = archive_paths(baseDir, yamlPaths['output-dir'].toString())
-			def outpath = mypaths['out-path'].replace('\\', '/')
-
-			// Archive the results
-			step([$class: 'JUnitResultArchiver', testResults: mypaths['xml-path']])
-			step([$class: 'ArtifactArchiver', artifacts: mypaths['out-files'], fingerprint: true])
+			// Find what to checkout, parse the run configuration
+			def cfgPaths = readYaml file: yaml_file
+			def yamlPaths = cleanPaths(baseDir, cfgPaths)
 			
+			// Archive the results
+			dir (yamlPaths['output-dir']) {
+				step([$class: 'JUnitResultArchiver', testResults: "results.xml"])
+				step([$class: 'ArtifactArchiver', artifacts: "*.*", fingerprint: true])
+			}
 			// Add a link to the report
-			currentBuild.description = """ <a href="artifact/${outpath}/index.html">Click here for HTML results</a> """
+			currentBuild.description = """ <a href="artifact/index.html">Click here for HTML results</a> """
 		}
 	}
 		
@@ -76,25 +69,25 @@ properties([[$class: 'BuildDiscarderProperty',
 		outDirStr = cfgPaths['MTT']['output-dir']
 		
 		if (thisDirStr == null || thisDirStr.isEmpty()) {
-			thisDir = Paths.get(baseDir + "/../Results").normalize().toString()
+			thisDir = baseDir + "/../Results"
 		} else {
-			thisDir = Paths.get(baseDir + "/" + thisDirStr).normalize().toString()			
+			thisDir = baseDir + "/" + thisDirStr
 		}
 
 		mapLocalPaths['this-dir'] = thisDir		
 		
 		if (mttResDirStr == null || mttResDirStr.isEmpty()) {
-			mttResDir = Paths.get(thisDir + "/../Results").normalize().toString()
+			mttResDir = thisDir + "/../Results"
 		} else {
-			mttResDir = Paths.get(baseDir + "/" + mttResDirStr	).normalize().toString()			
+			mttResDir = baseDir + "/" + mttResDirStr
 		}
 
 		mapLocalPaths['mtt-result-dir'] = mttResDir		
 		
 		if (outDirStr == null || outDirStr.isEmpty()) {
-			outDir = Paths.get(mttResDir + "/Output").normalize().toString()
+			outDir = mttResDir + "/Output"
 		} else {
-			outDir = Paths.get(baseDir + "/" + outDirStr).normalize().toString()
+			outDir = baseDir + "/" + outDirStr
 		}
 		
 		mapLocalPaths['output-dir'] = outDir
@@ -103,11 +96,10 @@ properties([[$class: 'BuildDiscarderProperty',
 	}
 	
 
-
-	def generateConvBatCall(baseDir, oct_home, mtt_home, mtt_egg, yaml_file) {
+	def generateConvBatCall(baseDir, mtt_egg, yaml_file) {
 		// Helper function to generate .bat script used to run tests
 		StringBuffer sb = new StringBuffer();		
-		sb = generateBatSetup(oct_home, mtt_home, mtt_egg)
+		sb = generateBatSetup(mtt_egg)
 		sb.append("call python -m mtt configure ")
 		sb.append("${baseDir}")
 		sb.append("\\")
@@ -117,24 +109,18 @@ properties([[$class: 'BuildDiscarderProperty',
 	}
 	
 	
-	def generateTestBatCall(baseDir, oct_home, mtt_home, mtt_egg) {
+	def generateTestBatCall(baseDir, mtt_egg) {
 		// Helper function to generate .bat script used to run tests
 		StringBuffer sb = new StringBuffer();
-		sb = generateBatSetup(oct_home, mtt_home, mtt_egg)
+		sb = generateBatSetup(mtt_egg)
 		sb.append("call python -m mtt run verify \n")
 		return sb.toString()
 	}
 	
 	
-	def generateBatSetup(oct_home, mtt_home, mtt_egg) {
+	def generateBatSetup(mtt_egg) {
 		// Helper function to generate .bat script used to run tests
-		StringBuffer sb = new StringBuffer();
-		sb.append("call ")
-		sb.append("${oct_home}")
-		sb.append("\\setenv.bat\n")
-		sb.append("set \"MTT_HOME=");
-		sb.append("${mtt_home}")
-		sb.append("\n");
+		StringBuffer sb = new StringBuffer();	
 		sb.append("set \"PATH=%MTT_HOME%;%PATH%\"\n");
 		sb.append("set \"PYTHONPATH=%MTT_HOME%;%PYTHONPATH%;%MTT_HOME%\\");
 		sb.append("${mtt_egg}")
@@ -145,30 +131,10 @@ properties([[$class: 'BuildDiscarderProperty',
 	
 	
 	@NonCPS
-	def archive_paths(baseDir, outDir) {
-		// Helper function to generate paths needed for output reports
-		def mypaths = [:]
-		def tmpPath = Paths.get(outDir,"results.xml")
-		def fullPath = Paths.get(baseDir)
-		def relPath = fullPath.relativize(tmpPath.normalize()).toString()
-		
-		mypaths['xml-path'] = relPath
-		
-		tmpPath = Paths.get(outDir)
-		relPath = fullPath.relativize(tmpPath.normalize()).toString()
-		
-		mypaths['out-path'] = relPath
-		mypaths['out-files'] = relPath + "\\**\\*.*"
-		
-		return mypaths
-	}
-	
-	
-	@NonCPS
 	def doCheckout(remoteLoc, branch, localLoc, credentials = '') {
 		// Helper function that performs a checkout from Git
 		echo "Checking out ${remoteLoc} to ${localLoc}"
-		checkout([$class: 'GitSCM', branches: scm.branches,
+		checkout([$class: 'GitSCM', branches: [[name: '*/' + branch]],
 			userRemoteConfigs: [[url: remoteLoc]]])
 	}	
 	
