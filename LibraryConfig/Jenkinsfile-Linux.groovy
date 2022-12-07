85591411c5669fbcdbf648d8d6fc7c3761cd8e5b
oloflanke <100685829+oloflanke@users.noreply.github.com>
2022-12-07 13:03:23 +0100

    Update Jenkinsfile-Linux.groovy

--- LibraryConfig/Jenkinsfile-Linux.groovy
+++ LibraryConfig/Jenkinsfile-Linux.groovy
@@ -1,29 +1,40 @@
-def mttImage = "/home/jenkins/images/mtt_oct_stable.tar"
-def mttImageName = "artifactory.modelon.com/mtt.oct.images/mtt_oct_stable_centos:latest"
+def mttImageName = "artifactory.modelon.com/aid.docker.dev.local/mtt/stable:latest"
 
-//def yaml_file="..\\ExampleTestLibrary\\LibraryConfig\\test_mtt_default.yaml"
+properties([pipelineTriggers([cron('H 6 * * 7')])])  // Just to ensure that it get run once a week
 
 node("Linux") {        
-stage('Checkout') {
-    dir("ExampleTestLibrary") {
-        checkout scm // defaults work nicely for git
-    }
-}
-try {
-    stage('Test') {
-        sh "docker load -i ${mttImage}"
-        def testingImage = docker.image(mttImageName)
-        def workspace = pwd()
-
-        testingImage.inside("-v ${workspace}:/job -w /RunDirectory") {
-            sh'''
-                export PYTHONPATH=:/usr/bin/mtt/mtt-3.0.0-py3.6.egg/::$PYTHONPATH
-                /usr/bin/oct/install/bin/jm_python.sh -m mtt configure /job/ExampleTestLibrary/LibraryConfig/test_mtt_default.yaml
-                /usr/bin/oct/install/bin/jm_python.sh -m mtt run verify
-            '''
+    stage('Checkout') {
+        dir("ExampleTestLibrary") {
+            checkout scm // defaults work nicely for git
         }
-
     }
+    try {
+        stage('Test') {
+            sh "docker pull ${mttImageName}"
+            def testingImage = docker.image(mttImageName)
+            def workspace = pwd()
+
+            dir("Results") {
+                deleteDir()
+            }
+            dir("RunDirectory") {
+                deleteDir()
+            }
+
+            testingImage.inside("-v ${workspace}:/job -w /RunDirectory") {
+            echo "Build info of current Docker image:"
+                sh'''
+                    cat /usr/bin/mtt/build_info.txt
+                    rpm -q modelon-mtt
+                    rpm -q modelon-oct
+
+                    export PYTHONPATH=:/usr/bin/mtt/mtt-3.0.0-py3.9.egg/::$PYTHONPATH
+                    /usr/bin/oct/install/bin/jm_python.sh -m mtt configure /job/ExampleTestLibrary/LibraryConfig/test_mtt_default.yaml
+                    /usr/bin/oct/install/bin/jm_python.sh -m mtt run verify
+                '''
+            }
+
+        }
     } finally {
         archiveArtifacts artifacts: 'Results/Output/**/*.*'
         archiveArtifacts artifacts: 'Results/SimulationResult/**', allowEmptyArchive: true
